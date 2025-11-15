ac4f2eb2300afbd84bab37b9cb4359a41bed243d
Paulo Daniel <paulodaniel1360@gmail.com>
2025-11-15 16:51:59 -0300

    Atualizando o site

--- assets/js/global/header.js
+++ assets/js/global/header.js
@@ -1,5 +1,23 @@
-$(document).ready(function() {
-    $('.responsive-btn').on('click', function() {
+$(document).ready(function () {
+    const $header = $('header');
+    const $responsiveBtn = $('.responsive-btn');
+    const $menuLinks = $('.menu-link'); // ajuste conforme sua classe real
+
+    // Toggle do menu mobile
+    $responsiveBtn.on('click', function () {
         $('.mobile-header').toggleClass('active');
+    });
+
+    // Efeito de scroll
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 50) {
+            $header.addClass('header-scrolled shadow-sm');
+            $responsiveBtn.addClass('main-color');
+            $menuLinks.addClass('text-black');
+        } else {
+            $header.removeClass('header-scrolled shadow-sm');
+            $responsiveBtn.removeClass('main-color');
+            $menuLinks.removeClass('text-black');
+        }
     });
 });
